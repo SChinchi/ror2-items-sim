0ffbf3546cced2a8bf316d87f77f11c8b6bc9377
SChinchi <57867641+SChinchi@users.noreply.github.com>
2026-09-23 16:46:56 +0100

    Add the scene name constants for Alloyed Collective

--- constants.py
+++ constants.py
@@ -11,14 +11,19 @@ class SceneName:
     WA = 'foggyswamp'           # Wetland Aspects
     AS = 'ancientloft'          # Aphelian Sanctuary
     RA = 'lemuriantemple'       # Reformed Altar
+    PP = 'nest'                 # Pretender's Precipice
     RD = 'frozenwall'           # Rallypoint Delta
     SA = 'wispgraveyard'        # Scorched Acres
     SP = 'sulfurpools'          # Sulfur Pools
     TC = 'habitat'              # Treeborn Colony
     GD = 'habitatfall'          # Golden Dieback
+    IA = 'ironalluvium'         # Iron Alluvium
+    IA2 = 'ironalluvium2'       # Iron Auroras
     AD = 'dampcavesimple'       # Abyssal Depths
     SC = 'shipgraveyard'        # Siren's Call
     SG = 'rootjungle'           # Sundered Grove
+    RP = 'repurposedcrater'     # Repurposed Crater
+    CC = 'conduitcanyon'        # Conduit Canyon
     SM = 'skymeadow'            # Sky Meadow
     HH = 'helminthroost'        # Helminth Hatchery
     CO = 'moon2'                # Commencement
@@ -33,6 +38,9 @@ class SceneName:
     MF = 'mysteryspace'         # A Moment, Fractured
     MW = 'limbo'                # A Moment, Whole
     PM = 'meridian'             # Prime Meridian
+    SH = 'solutionalhaunt'      # Solutional Haunt
+    CE = 'computationalexchange'# Computational Exchange
+    NS = 'solusweb'             # Neural Sanctum
     # Simulacrum
     STP = 'itgolemplains'       # Simulacrum, Titanic Plains
     SAS = 'itancientloft'       # Simulacrum, Aphelian Sanctuary
@@ -60,7 +68,7 @@ class Expansion:
     AC = 'DLC3'
 
 
-ALL_EXPANSIONS = set([Expansion.SOTV, Expansion.SOTS, Expansion.AC])
+ALL_EXPANSIONS = {Expansion.SOTV, Expansion.SOTS, Expansion.AC}
 NO_EXPANSIONS = set()
 
 IT_STAGES = (
